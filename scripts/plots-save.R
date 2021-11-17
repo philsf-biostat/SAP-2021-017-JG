6c2be74006553c21d5cc83a137f4fd0e804e232d
Felipe Figueiredo <philsf79@gmail.com>
2021-11-17 20:53:55 -0300

    tamanhos e resoluções ajustadas

--- scripts/plots-save.R
+++ scripts/plots-save.R
@@ -15,10 +15,10 @@ units <- "cm"
 
 ggsave(filename = "figures/hiper.png", plot = gg.hiper, height = 16, width = 16, units = units)
 
-png("figures/sil_final.png", width = 16, height = 16, units = units, res = 600)
+png("figures/sil_final.png", width = 16, height = 14, units = units, res = 600)
 final_sil %>% plot(main="")
 dev.off()
 
-png("figures/sil_rej.png", width = 16, height = 12, units = units, res = 600)
+png("figures/sil_rej.png", width = 14, height = 12, units = units, res = 600)
 final_sil.rej %>% plot(main="")
 dev.off()
