18b996325fa1c08d93527123ce306fb0e12154fa
Felipe Figueiredo <philsf79@gmail.com>
2021-11-16 21:24:45 -0300

    input importado de SAR-2021-011

--- scripts/input.R
+++ scripts/input.R
@@ -1,37 +1,62 @@
 # setup -------------------------------------------------------------------
-# library(data.table)
 library(tidyverse)
-library(readxl)
-# library(lubridate)
 library(labelled)
 
 # data loading ------------------------------------------------------------
 set.seed(42)
-data.raw <- tibble(id=gl(2, 10), group = gl(2, 10), outcome = rnorm(20))
-# data.raw <- read_excel("dataset/file.xlsx") %>%
-#   janitor::clean_names()
-
+data.raw <- read_csv("dataset/eleitos_2018.csv") %>%
+  janitor::clean_names()
 
 # data cleaning -----------------------------------------------------------
 
 data.raw <- data.raw %>%
-  # select() %>%
-  mutate() %>%
-  filter()
+  #   filter() %>%
+  select(
+    -eleito,
+  ) %>%
+  # dropar variáveis com NA que não podem ser usadas na clusterização
+  drop_na(num_votos)
 
 # data wrangling ----------------------------------------------------------
 
 data.raw <- data.raw %>%
   mutate(
-    id = factor(id), # or as.character
+    id = as.character(id),
+    primeira = factor(primeira, labels = c("Primeiro mandato", "Reeleito")),
+    sexo = factor(sexo, labels = c("Masculino", "Feminino")),
+    evangelico = factor(evangelico, labels = c("Outros", "Evangélico")),
+    igreja = fct_rev(fct_infreq(igreja)),
+    total_receita = total_receita/1000000,
+    num_votos = num_votos/1000000,
+    filiados = filiados/1000000,
+    receita_agp = receita_agp/1000000,
+    receita_agr = receita_agr/1000000,
+    receita_com = receita_com/1000000,
+    receita_fin = receita_fin/1000000,
+    receita_inf = receita_inf/1000000,
+    receita_ind = receita_ind/1000000,
+    receita_pf = receita_pf/1000000,
+    receita_rp = receita_rp/1000000,
+    receita_ser = receita_ser/1000000,
+    # capilaridade = capilaridade*10,
   )
 
 # labels ------------------------------------------------------------------
 
 data.raw <- data.raw %>%
   set_variable_labels(
-    group = "Study group",
-    outcome = "Study outcome",
+    partido = "Partido",
+    uf = "UF",
+    capilaridade = "Capilaridade",
+    primeira = "Releição vs primeiro mandato",
+    sexo = "Sexo",
+    evangelico = "Evangélico",
+    num_votos = "Votos (milhão)",
+    decil_filiados = "Decil do núm. de filiados",
+    decil_deputados = "Decil do núm. de deputados",
+    total_receita = "Receita total (milhão R$)",
+    posicao = "Índice de Power e Silveira-Rodrigues",
+    igreja = "Nome da Igreja",
   )
 
 # analytical dataset ------------------------------------------------------
@@ -39,9 +64,12 @@ data.raw <- data.raw %>%
 analytical <- data.raw %>%
   # select analytic variables
   select(
-    id,
-    group,
-    outcome,
+    !starts_with(c("perc", "receita_")),
+    -nome,
+    -igreja,
+    -categoria,
+    -filiados,
+    # -primeira,
   )
 
 # mockup of analytical dataset for SAP and public SAR
